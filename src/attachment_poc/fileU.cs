3f7ca93c2b0dd274fe34f5eeba532624bd8a515f
trupti22 <trupti.girde@rgensoft.com>
2016-10-17 17:04:23 +0530

    Add files via upload

---
+++ src/attachment_poc/fileU.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace attachment_poc
+{
+    public class fileU
+    {
+        public string Name { get; set; }
+        public string ContentType { get; set; }
+        public string Data { get; set; }
+
+    }
+    
+}
