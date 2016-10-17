6735766c82f86b772909105adadb4fd8b0eb4158
trupti22 <trupti.girde@rgensoft.com>
2016-10-17 16:59:17 +0530

    Update HomeController.cs

--- src/attachment_poc/Controllers/HomeController.cs
+++ src/attachment_poc/Controllers/HomeController.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,6 +9,10 @@ using Microsoft.Net.Http.Headers;
 using System.IO;
 using System.Runtime;
 using System.Text.RegularExpressions;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+using System.Net.Http;
 
 namespace attachment_poc.Controllers
 {
@@ -22,42 +26,106 @@ namespace attachment_poc.Controllers
         {
             this.hostingEnv = env;
         }
+
         public IActionResult UploadFiles()
         {
+            String connectionString = "Data Source = 203.193.138.52,6148; Initial Catalog = POC_UAT; Persist Security Info = True; User ID = ttsh_crio; Password = Newuser@123";
+            String sql = "SELECT * FROM fileUpload";
+            SqlCommand cmd = new SqlCommand(sql);
+            var model = new List<fileU>();
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                cmd.Connection = conn;
+                SqlDataReader rdr = cmd.ExecuteReader();
+                
+                while (rdr.Read())
+                {
+                    var fileInfo = new fileU();
+                    fileInfo.Name = rdr["Name"].ToString();
+                    byte[] bytes = (byte[])rdr["Data"];
+
+                    string base64String = Convert.ToBase64String(bytes, 0, bytes.Length);
+                    fileInfo.Data = "data:image/png;base64," +base64String;
+                    
+                    model.Add(fileInfo);
+
+                    //Response.ContentType = rdr["ContentType"].ToString();          
+                    //Response.Headers.Add("content-disposition", "attachment;filename=" + fileInfo.Name);
+                    //Response.Body.WriteAsync(bytes, 0, bytes.Length);
+                }
+            }            
+            return View(model);
+        }
+
+        public IActionResult download()
+        {
+            String connectionString = "Data Source = 203.193.138.52,6148; Initial Catalog = POC_UAT; Persist Security Info = True; User ID = ttsh_crio; Password = Newuser@123";
+            String sql = "SELECT * FROM fileUpload where id = 1";
+            SqlCommand cmd = new SqlCommand(sql);
+            var model = new List<fileU>();
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                cmd.Connection = conn;
+                SqlDataReader rdr = cmd.ExecuteReader();
+
+                while (rdr.Read())
+                {
+                    var fileInfo = new fileU();
+                    fileInfo.Name = rdr["Name"].ToString();
+                    byte[] bytes = (byte[])rdr["Data"];
+
+                    string base64String = Convert.ToBase64String(bytes, 0, bytes.Length);
+                    fileInfo.Data = "data:image/png;base64," + base64String;
+
+                    model.Add(fileInfo);
+
+                    Response.ContentType = rdr["ContentType"].ToString();          
+                    Response.Headers.Add("content-disposition", "attachment;filename=" + fileInfo.Name);
+                    Response.Body.WriteAsync(bytes, 0, bytes.Length);
+                }
+            }
             return View();
         }
 
-
-       
-        //  foreach(var file in Request.Files)
-        //
         [HttpPost]
         public IActionResult UploadFiles(IList<IFormFile> files)
-        {          
+        {
             foreach (var file in files)
             {
-                var filename = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName.Trim('"');
-                var fileNameTrim = "";
-                /*Solution for IE issue*/
-                int position = filename.LastIndexOf('\\');
+                string filename = Path.GetFileName(file.FileName);
+                string contentType = file.ContentType;
 
-                if (position != -1)
-                    fileNameTrim = filename.Substring(position + 1);
-                else
-                    fileNameTrim = filename;
-                /**/
-               // filename = hostingEnv.WebRootPath + $@"\Attchment\{fileNameTrim}";
-                filename = hostingEnv.ContentRootPath +$@"\Uploads\{fileNameTrim}";
-                using (FileStream fs=System.IO.File.Create(filename))
+                using (var reader = new StreamReader(file.OpenReadStream()))
                 {
-                    file.CopyTo(fs);
-                    fs.Flush();
-                }
-            }
+                    var fileContent = reader.ReadToEnd();
+                   
+                    BinaryReader br = new BinaryReader(file.OpenReadStream());
+                    byte[] bytes = br.ReadBytes((Int32)fileContent.Length);
+
+                    using (SqlConnection con = new SqlConnection("Data Source =192.168.1.152,32768; Initial Catalog =RGenUAT; Persist Security Info =True;User ID=uatadmin;Password=Newuser@123"))
+                    {
+                        string query = "insert into fileUpload values (@Name, @ContentType, @Data)";
+                        using (SqlCommand cmd = new SqlCommand(query))
+                        {
+                            cmd.Connection = con;
+                            cmd.Parameters.AddWithValue("@Name", filename);
+                            cmd.Parameters.AddWithValue("@ContentType", contentType);
+                            cmd.Parameters.AddWithValue("@Data", bytes);
+                            con.Open();
+                            cmd.ExecuteNonQuery();
+                            con.Close();
+                        }
+                    }
+                }               
+            }        
             return View();        
         }
         /**********/
-
+        
         public IActionResult Index()
         {
             return View();
